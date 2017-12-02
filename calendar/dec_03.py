0885ed6f028add25122f31310f51040aa67dbd07
Menturan <ohlander91@gmail.com>
2017-12-02 12:01:09 +0100

    Ready for dec 3!

---
+++ calendar/dec_03.py
@@ -0,0 +1,24 @@
+from christmas_tree import tree
+
+
+def part1():
+    """
+Puzzle
+    :return sum:
+    """
+    pass
+
+
+def part2():
+    """
+Puzzle
+    :return sum:
+    """
+    pass
+
+
+print(tree)
+print('### Part 1 ###')
+print('Resultat: ' + str(part1()))
+print('### Part 2 ###')
+print('Resultat: ' + str(part2()))
