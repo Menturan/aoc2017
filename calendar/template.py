6158ed823d4e837bc304a0a87c9010f62f05cff2
Menturan <ohlander91@gmail.com>
2017-12-02 11:54:47 +0100

    Updated template with docs.

--- calendar/template.py
+++ calendar/template.py
@@ -2,10 +2,18 @@ from christmas_tree import tree
 
 
 def part1():
+    """
+Puzzle
+    :return sum:
+    """
     pass
 
 
 def part2():
+    """
+Puzzle
+    :return sum:
+    """
     pass
 
 
@@ -14,6 +22,3 @@ print('### Part 1 ###')
 print('Resultat: ' + str(part1()))
 print('### Part 2 ###')
 print('Resultat: ' + str(part2()))
-
-
-
