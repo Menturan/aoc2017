db683e613843263f348748eb1c50c0667146a597
Menturan <ohlander91@gmail.com>
2017-12-23 12:14:59 +0100

    Dec 5 Done!

--- calendar/dec_05.py
+++ calendar/dec_05.py
@@ -16,32 +16,49 @@ Puzzle
                 no_of_jumps += 1
                 continue
             else:
-                position += jumps[position]
+                value_at_current_position = jumps[position]
                 jumps[position] += 1
+                position += value_at_current_position
                 no_of_jumps += 1
         except IndexError:
-            no_of_jumps += 1
             break
     return no_of_jumps
 
 
 def part1_test():
     test1 = part1([0, 3, 0, 1, -3])
-    assert test1 == 5, 'Returnerade inte 5 var {}.'.format(test1)
+    assert test1 == 5, 'Summan är inte 5 var {}.'.format(test1)
 
 
-
-def part2(input) -> int:
+def part2(jumps) -> int:
     """
 Puzzle
     :return sum:
     """
-
-    pass
+    no_of_jumps = 0
+    position = 0
+    while True:
+        try:
+            if jumps[position] == 0:
+                jumps[position] += 1
+                no_of_jumps += 1
+                continue
+            else:
+                value_at_current_position = jumps[position]
+                if value_at_current_position >= 3:
+                    jumps[position] -= 1
+                else:
+                    jumps[position] += 1
+                position += value_at_current_position
+                no_of_jumps += 1
+        except IndexError:
+            break
+    return no_of_jumps
 
 
 def part2_test():
-    pass
+    test1 = part2([0, 3, 0, 1, -3])
+    assert test1 == 10, 'Summan är inte 10 var {}.'.format(test1)
 
 
 print(tree)
@@ -50,4 +67,4 @@ part1_test()
 print('Resultat: ' + str(part1(read_file_to_list_of_numbers('dec_05_jumps.txt'))))
 print('### Part 2 ###')
 part2_test()
-print('Resultat: ' + str(part2("")))
+print('Resultat: ' + str(part2(read_file_to_list_of_numbers('dec_05_jumps.txt'))))
